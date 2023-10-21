77217a42564b0d18ac31512ca4854a30a70c8575
admon84 <admonwest84@gmail.com>
2023-10-20 22:43:12 -0600

    auto remove bots on server init

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+const utils = require.main.require('@screeps/backend/lib/utils.js')
+
 const INVADER_ID = '2';
 
-module.exports = function engine(config) {
+module.exports = (config) => {
   if (config.engine) {
-    config.engine.on('processRoom', function (roomId, roomInfo, roomObjects, roomTerrain, gameTime, objectsUpdate, usersUpdate, eventLog) {
+    // Remove bots
+    config.engine.on('init', async type => {
+      if (type === 'main') {
+        const { db, env } = config.common.storage;
+        const bots = await db.users.find({ bot: 'simplebot' });
+        for (const { _id, rooms, username } of bots) {
+          console.log(`Removing bot ${username} from ${rooms[0]}.`);
+          await utils.respawnUser(_id);
+          await db.users.removeWhere({ _id: _id });
+          await db['users.code'].removeWhere({ user: _id });
+          await env.del(env.keys.MEMORY + _id);
+          await env.del(env.keys.MEMORY_SEGMENTS + _id);
+        }
+      }
+    });
+
+    // Remove invaders
+    config.engine.on('processRoom', (roomId, roomInfo, roomObjects, roomTerrain, gameTime, objectsUpdate, usersUpdate, eventLog) => {
       if (!roomObjects) return;
       for (const object of Object.values(roomObjects)) {
-        if (object.user == INVADER_ID) {
+        if (object.user === INVADER_ID) {
           console.log(`Removing invader ${object.type} (${object._id}) from ${roomId} at ${gameTime}.`);
           objectsUpdate.remove(object._id);
         }
