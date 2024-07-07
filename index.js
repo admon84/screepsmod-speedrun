5aadd526ecf56cd227d0dc6a456024b5e703eb1c
admon84 <admonwest84@gmail.com>
2024-07-07 13:54:26 -0600

    refactor

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 const utils = require.main.require('@screeps/backend/lib/utils.js')
 
-module.exports = (config) => {
-  // Remove bots on server init
+const MOD_NAME = '[screepsmod-speedrun]'
+
+module.exports = config => {
   if (config.engine) {
     config.engine.on('init', async type => {
       if (type === 'main') {
-        const { db, env } = config.common.storage;
-        const bots = await db.users.find({ bot: 'simplebot' });
-        for (const { _id, rooms, username } of bots) {
-          console.log(`Removing bot ${username} from ${rooms[0]}.`);
-          await utils.respawnUser(_id);
-          await db.users.removeWhere({ _id });
-          await db['users.code'].removeWhere({ user: _id });
-          await env.del(env.keys.MEMORY + _id);
-          await env.del(env.keys.MEMORY_SEGMENTS + _id);
-        }
+        await removeBots(config)
       }
-    });
+    })
   }
 
-  // Remove cronjobs
   if (config.cronjobs) {
-    console.log(`Removing stronghold and invader cronjobs.`);
-    delete config.cronjobs.genInvaders;
-    delete config.cronjobs.genStrongholds;
-    delete config.cronjobs.expandStrongholds;
+    console.log(`${MOD_NAME} disabling strongholds and invaders`)
+    delete config.cronjobs.genInvaders
+    delete config.cronjobs.genStrongholds
+    delete config.cronjobs.expandStrongholds
   }
-};
+}
+
+async function removeBots(config) {
+  const { db, env } = config.common.storage
+
+  if (!db) {
+    console.log(`${MOD_NAME} no database found, skipping`)
+    return
+  }
+
+  if (!db.users) {
+    console.log(`${MOD_NAME} no users collection found, skipping`)
+    return
+  }
+
+  // Target NPC bots for removal
+  const bots = await db.users.find({ bot: { $exists: true } })
+
+  for (const { _id, username } of bots) {
+    console.log(`${MOD_NAME} removing bot "${username}"`)
+    // Respawn the bot user (removes creeps, structures, construction sites, etc.)
+    await utils.respawnUser(_id)
+    // Remove the bot user from the database
+    await db.users.removeWhere({ _id })
+    await db['users.code'].removeWhere({ user: _id })
+    await env.del(env.keys.MEMORY + _id)
+    await env.del(env.keys.MEMORY_SEGMENTS + _id)
+  }
+}
